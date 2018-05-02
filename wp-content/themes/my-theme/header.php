27d1e20015e4807ad95993722fd7fd84ac96f34b
Alessia Loddo <33028423+alessiameeko@users.noreply.github.com>
2018-05-02 13:19:51 +0200

    Update header.php

--- wp-content/themes/my-theme/header.php
+++ wp-content/themes/my-theme/header.php
@@ -1,1 +1,11 @@
-
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+      <head>
+      <meta charset="utf-8">
+      <title>ORO PURO</title>
+</head>
+<body>
+  <h3>Home Dove siamo I nostri gioielli Contatti</h3>
+  <hr>
+  
+    
