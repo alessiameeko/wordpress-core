f27cbf0562b00f61c6d20e7c54bea08fc8d47c68
Alessia Loddo <33028423+alessiameeko@users.noreply.github.com>
2018-05-02 15:06:59 +0200

    Update index.php

--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -1,4 +1,9 @@
 <?php get_header();?>
-<h1>Alessia's wp theme</h1>
-<p>Laboratorio artigiano e gioielleria</p>
+<div class="jumbotron">
+  <h1 class="display-4"><php bloginfo('name');?></h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information</p>
+      <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container,<p/>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
 <?php get_footer();?>
